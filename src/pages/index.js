18d06a3ca9525c5e3552e41b48de3703e75d3bdc
R B <rons9n@gmail.com>
2021-05-19 18:43:17 -0400

    Assests added

---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { ThemeProvider } from 'theme-ui';
+// import theme from 'theme';
+
+export default function IndexPage() {
+  return (
+        <h1>Nextop</h1>
+  );
+}
