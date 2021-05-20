f5b4f40307baa9bc1e91b6e42d04a3e7a1c67f95
R B <rons9n@gmail.com>
2021-05-20 14:00:12 -0400

    Document.js file added

---
+++ src/pages/_document.js
@@ -0,0 +1,21 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class CustomDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html lang="en-US">
+        <Head />
+        <body>
+          <h1>Nextop</h1>
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default CustomDocument;
